3bd28007bf85b1fdb1d28c5db913fbdf80b29118
spandanb <spandan.777@gmail.com>
2014-05-26 19:47:42 -0400

    repo and utils script created

---
+++ utils.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+#Create bash_aliases file
+bash_aliases="~/.bash_aliases"
+if [ -f $bash_aliases ]; then
+    . $bash_aliases
+else
+    touch $bash_aliases
+fi
+
+#Create git completion file 
+if [ ! -f ~/.git-completion.bash ]; then
+    cd ~
+    wget -O ~/.git-completion.bash https://raw.githubusercontent.com/git/git/master/contrib/completion/git-completion.bash
+fi
+
+#Overwrites any existing $bash_aliases file
+cat > $bash_aliases << "EOF"
+#!/bin/bash
+
+alias reload="source ~/.bashrc"
+alias profile="vim ~/.bash_aliases"
+
+#git helper aliases
+alias gr="git remote -v" #List
+alias gf="git fetch"
+alias gb="git branch -a"
+alias gc="git checkout s-havana"
+EOF
+
+#reload bashrc file
+source ~/.bashrc
+
