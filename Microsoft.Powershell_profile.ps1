771b0a5f48e727a006e48a63aa2179c659c728d7
Spandan Bemby <spandan.777@gmail.com>
2020-05-20 16:23:39 -0400

    added faves and bash-like autocomplete

--- Microsoft.Powershell_profile.ps1
+++ Microsoft.Powershell_profile.ps1
@@ -29,6 +29,8 @@ function BackOneDir{
 }
 Set-Alias bd BackOneDir
 
+# bash like autocomplete on Tab
+Set-PSReadlineKeyHandler -Key Tab -Function Complete
 
 function pro {
 	param(
@@ -50,7 +52,17 @@ function q { python $args }
 function gs { git status }
 function gd { git diff }
 function gb { git branch }
-Set-Alias -Name "ag" -Value "findstr"
+
+function groot {
+    # cd to git root
+    # taken from: https://stackoverflow.com/a/957978
+	cd (git rev-parse --show-toplevel)
+}
+
+function ag {
+    # usage: ag foo bar car
+    findstr /spin /c:"$args" *
+}
 
 function nixPath {
     # convert windows path to linux path
@@ -65,4 +77,42 @@ function tig {
     # linux paths
 	$argument = nixPath $argument
     bash -c "tig $argument"
+}
+
+function faves {
+	# print list of favorite_dirs, like [0] pa/th [0]
+    # usage: faves                    # lists
+    #        faves -jmp 0 | faves 0   # jump to 0th dir
+
+	# TODO: the list of fave dirs could be dynamically updated
+	# either by parsing PS history file
+	# or explicitly tracking directory changes
+	# keep track of last N histories visited
+
+	param(
+	[string] $jmp = $null,
+	[switch] $help = $false
+	)
+
+	# list of favorite dirs
+	$favedirs = @(
+		"C:\",
+        "C:\Users\spandan",
+	)
+
+
+	if (![string]::IsNullOrEmpty($jmp)) {
+		write-output "jumping to $jmp"
+		cd $favedirs[$jmp]
+	}
+	elseif ($help){
+			write-output "Usage: faves [-help | -jmp num] (no-arg lists faves)"
+	}
+	else{
+		# output the dirs
+		for($i = 0; $i -le $favedirs.count -1; $i++) {
+			$dir = $favedirs[$i]
+			write-output "[$i]:  $dir [$i]"
+		}
+	}
 }
